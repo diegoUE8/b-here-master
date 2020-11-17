82f3d4a76e21406a86d64bc7e5ec98282f17fa48
Diego <30624958+diegoUE8@users.noreply.github.com>
2020-11-17 18:02:40 +0100

    com fix

--- docs/js/main.js
+++ docs/js/main.js
@@ -189,7 +189,7 @@ function _readOnlyError(name) {
   },
   assets: './',
   worker: './js/workers/image.service.worker.js',
-  githubDocs: 'https://raw.githubusercontent.com/diegoUE8/b-here/heroesvalley/docs/',
+  githubDocs: 'https://raw.githubusercontent.com/diegoUE8/b-here-master/heroesvalley/docs/',
   url: {
     index: '/',
     access: '/',
