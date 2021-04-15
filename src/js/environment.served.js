5062208f2ac204a8ac7d672ece7dd7c39cf52b01
Diego <30624958+diegoUE8@users.noreply.github.com>
2021-04-15 11:57:57 +0200

    first com

--- src/js/environment.served.js
+++ src/js/environment.served.js
@@ -41,7 +41,7 @@ export const environmentServed = {
 	},
 	assets: '/Modules/B-Here/Client/docs/',
 	worker: '/Modules/B-Here/Client/docs/js/workers/image.service.worker.js',
-	githubDocs: 'https://raw.githubusercontent.com/diegoUE8/b-here-master/ws/docs/',
+	githubDocs: 'https://raw.githubusercontent.com/diegoUE8/b-here-master/cr.bhere/docs/',
 	template: {
 		tryInAr: '/template/modules/b-here/try-in-ar.cshtml?viewId=$viewId',
 		modal: {
