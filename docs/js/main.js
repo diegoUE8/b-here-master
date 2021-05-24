f0baee8e3f97d574cb159d771f4eac5270e69448
Diego <30624958+diegoUE8@users.noreply.github.com>
2021-05-24 11:43:30 +0200

    save 2

--- docs/js/main.js
+++ docs/js/main.js
@@ -122,7 +122,7 @@ function _assertThisInitialized(self) {
 function _readOnlyError(name) {
   throw new Error("\"" + name + "\" is read-only");
 }var environmentServed = {
-  appKey: '8b0cae93d47a44e48e97e7fd0404be4e',
+  appKey: '865af1430a854af5b01733ff9b725a2b',
   channelName: 'BHere',
   flags: {
     production: true,
@@ -195,11 +195,11 @@ function _readOnlyError(name) {
     }
   }
 };var environmentStatic = {
-  appKey: '8b0cae93d47a44e48e97e7fd0404be4e',
+  appKey: '865af1430a854af5b01733ff9b725a2b',
   channelName: 'BHere',
   flags: {
     production: false,
-    useProxy: true,
+    useProxy: false,
     useToken: false,
     selfService: true,
     guidedTourRequest: true,
@@ -297,7 +297,7 @@ var PARAMS = NODE ? {
 var DEBUG =  PARAMS.get('debug') != null;
 var BASE_HREF = NODE ? null : document.querySelector('base').getAttribute('href');
 var HEROKU = NODE ? false : window && window.location.host.indexOf('herokuapp') !== -1;
-var STATIC = NODE ? false : HEROKU || window && (window.location.port === '41789' || window.location.port === '5000' || window.location.port === '6443' || window.location.host === 'actarian.github.io');
+var STATIC = NODE ? false : HEROKU || window && (window.location.port === '41789' || window.location.port === '5000' || window.location.port === '6443' || window.location.host === 'diegoUE8.github.io');
 var DEVELOPMENT = NODE ? false : window && ['localhost', '127.0.0.1', '0.0.0.0'].indexOf(window.location.host.split(':')[0]) !== -1;
 var PRODUCTION = !DEVELOPMENT;
 var ENV = {
